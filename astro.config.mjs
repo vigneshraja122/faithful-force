f4b82131bd1909bc402bade281204395a76ce2d6
vigneshraja122 <vraja3868@gmail.com>
2024-11-13 14:16:23 +0530

    Added sample template to the landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import sharp from 'sharp';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  assets: {
+    addSharpInstructions: true,
+    services: [
+      {
+        name: 'sharp',
+        constructor: sharp,
+      },
+    ],
+  },
+});
