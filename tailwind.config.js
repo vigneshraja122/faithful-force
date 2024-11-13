039b6a469661c4dc4cc609b07a530ad4e1aafb4d
vigneshraja122 <vraja3868@gmail.com>
2024-11-13 18:59:19 +0530

    Added css to the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
+import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme'
+
+export const content = ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}']
+
+export const theme = {
+  extend: {
+    colors: {
+      primary: {
+        100: '#E6F0FF',
+        500: '#0066CC',
+        600: '#0052A3',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', ..._fontFamily.sans],
+      serif: ['Merriweather', ..._fontFamily.serif],
+    },
   },
-  plugins: [],
 }
 
